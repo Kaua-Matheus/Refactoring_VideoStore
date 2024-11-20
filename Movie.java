443f026c7fe66bb8e40e49b6b88d6a42cc53941e
Kaua_Matheus <kmsilva3@minha.fag.edu.br>
2024-11-20 15:36:04 -0300

    Commit 4

--- Movie.java
+++ Movie.java
@@ -1,3 +1,4 @@
+
 public class Movie {
 
   public static final int  CHILDRENS = 2;
--- Rental.java
+++ Rental.java
@@ -1,3 +1,4 @@
+
 public class Rental {
 
     private Movie _movie;
@@ -14,5 +15,31 @@ public class Rental {
  
     public Movie getMovie() {
        return _movie;
+    }
+
+    public double getCharge() {
+
+        double thisAmount = 0;
+        
+        //determine amounts for aRental line
+        switch (this.getMovie().getPriceCode()) 
+        {
+            case Movie.REGULAR:
+            thisAmount += 2;
+                if (this.getDaysRented() > 2)
+                    thisAmount += (this.getDaysRented() - 2) * 1.5;
+            break;
+
+            case Movie.NEW_RELEASE:
+                thisAmount += this.getDaysRented() * 3;
+            break;
+
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (this.getDaysRented() > 3)
+                thisAmount += (this.getDaysRented() - 3) * 1.5;
+            break;
+        };
+        return thisAmount;
     }
  }
